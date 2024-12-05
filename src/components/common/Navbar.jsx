64a6cd08dc4fb1e933a9cc2d12c7925e983107ab
shamimranapro85 <shamimranaprofessional@gmail.com>
2024-12-05 19:52:25 +0600

    navbar blur increment

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -63,7 +63,7 @@ const navigate = useNavigate()
   };
   return (
     <>
-      <div className="navbar sticky top-0 z-50 backdrop-blur-sm ">
+      <div className="navbar sticky top-0 z-50 backdrop-blur-md ">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
